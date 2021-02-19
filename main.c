3e48679111f58b0276e5d7d13e3f2ce426c5fb08
Connor Bentzley <bentzlec@os1.engr.oregonstate.edu>
2021-02-19 10:21:13 -0800

    Did pretty much nothing yet

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define SIZE 10
+
+#define NUM_ITEMS 6
+
+int buff1[SIZE];
+int buff2[SIZE];
+int buff3[SIZE];
+
+int main() {
+
+
+
+
+
+
+
+
+
+
+   return 0;
+}
