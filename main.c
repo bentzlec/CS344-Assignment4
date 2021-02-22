e53cf9efe44bc339bd0348668dc80cbc7192f7c2
Connor Bentzley <bentzlec@os1.engr.oregonstate.edu>
2021-02-22 12:55:45 -0800

    Pretty much finished, just need to comment code

--- main.c
+++ main.c
@@ -11,61 +11,66 @@ char buff1[NUM_LINES][SIZE] = {{0}};
 char buff2[NUM_LINES][SIZE] = {{0}};
 char buff3[NUM_LINES][SIZE] = {{0}};
 
-int line1, line2, line3 = 0; //How many lines in each buffer
+int line1, line2, line3 = 0; //How many lines in each buffer, basically an item counter for each buffer
 
-int con_idx1, con_idx2, con_idx3 = 0;
+int con_idx1, con_idx2, con_idx3 = 0; //Consumer index
 
-int stop = 0;
+int stop = 0; //Flag for when "STOP\n" is read
 
-pthread_mutex_t mutex_1, mutex_2, mutex_3 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_1, mutex_2, mutex_3 = PTHREAD_MUTEX_INITIALIZER; //Took these names from the example code and heavily based all of my code on: https://repl.it/@cs344/65prodconspipelinec
 
 pthread_cond_t full_1, full_2, full_3 = PTHREAD_COND_INITIALIZER;
 
 
-/*************************************************
- ******* ACTIONS PERFORMED BY THREAD 4 START *****
- ************************************************/
-char * get_buff3() {
-   char * line = malloc(strlen(buff3[con_idx3]) + 1);
-   strcpy(line, buff2[con_idx3]);
-   con_idx3++;
+/****************************
+ ******* THREAD 4 START *****
+ ***************************/
+char * get_buff3() {                                 
+   char * line = malloc(strlen(buff3[con_idx3]) + 1); //Allocate line to hold what we grab from the buffer
+   strcpy(line, buff2[con_idx3]); 
+   con_idx3++; //Iterate consumer index
    
    return line;
 }
 
-void * printBuff() {
-   int last_line = 0;
-   char output[81] = {0};
-   char * print_buffer = calloc(SIZE * NUM_LINES, sizeof(char));
-   char * line = NULL;
+void * printBuff() {  //This is heavily based on Michael Slater's Pseudocode on Piazza: https://piazza.com/class/kjc3320l16c2f1?cid=465
+   int last_line = 0; //Keep track of what has been printed
+   char output[81] = {0}; //Buffer that will be printed to screen
+   char * print_buffer = calloc(SIZE * NUM_LINES, sizeof(char)); //Load all the lines into one long line
+   char * line = NULL; //Use this
+
    pthread_mutex_lock(&mutex_3);
-   while(line3 == 0) {
+
+   while(line3 == 0) { //If there's no items in buffer, wait
       pthread_cond_wait(&full_3, &mutex_3);
    }
-   for(int i = 0; i < line3; i++) {
+
+   for(int i = 0; i < line3; i++) { //Load buffer 3 into the one big buffer
       line = get_buff3();
       strcat(print_buffer, line);
    }
+
    pthread_mutex_unlock(&mutex_3);
 
-   int curr_line_count = (strlen(print_buffer) / 80);
+   int curr_line_count = (strlen(print_buffer) / 80); //Figure out how many complete lines we can print
 
-   for(int t = 0; t < curr_line_count; t++) {
-      memset(output, 0, 81);
+   //Load 80 characters into the output buffer
+   for(int t = 0; t < curr_line_count; t++) { 
+      memset(output, 0, 81);                 
       strncpy(output, print_buffer + (t * 80), 80);
       printf("%s\n", output);
       fflush(stdout);
    }
    last_line = curr_line_count;
 }
-/*************************************************
- ******* ACTIONS PERFORMED BY THREAD 4 END *****
- ************************************************/
+/**************************
+ ******* THREAD 4 END *****
+ *************************/
 
 
- /*************************************************
- ******* ACTIONS PERFORMED BY THREAD 3 START *****
- ************************************************/
+ /*************************
+ ***** THREAD 3 START *****
+ *************************/
 char * get_buff2() {
    char * line = malloc(strlen(buff2[con_idx2]) + 1);
    strcpy(line, buff2[con_idx2]);
@@ -90,11 +95,9 @@ void convertPlus(char * userInput) {
    sprintf(userInput, buffer, '^');
 }
 
-
 void * changePlus() {
    char * line = NULL;
    char * temp = NULL;
-   printf("Items in buffer 2: %i\n", line2);
 
    pthread_mutex_lock(&mutex_3);
    while(line2 == 0) {
@@ -108,15 +111,15 @@ void * changePlus() {
    pthread_cond_signal(&full_3);
    pthread_mutex_unlock(&mutex_3);
 }
-/*************************************************
- ******* ACTIONS PERFORMED BY THREAD 3 END *****
- ************************************************/
+/**************************
+ ******* THREAD 3 END *****
+ *************************/
 
 
 
-/*************************************************
- ******* ACTIONS PERFORMED BY THREAD 2 START *****
- ************************************************/
+/****************************
+ ******* THREAD 2 START *****
+ ***************************/
 char * get_buff1() {
    char * line = malloc(strlen(buff1[con_idx1]) + 1);
    strcpy(line, buff1[con_idx1]);
@@ -143,15 +146,15 @@ void * separateLine() {
       pthread_mutex_unlock(&mutex_2);
    }
 }
-/***********************************************
- ******* ACTIONS PERFORMED BY THREAD 2 END *****
- **********************************************/
+/**************************
+ ******* THREAD 2 END *****
+ *************************/
 
 
 
-/*************************************************
- * ***** ACTIONS PERFORMED BY THREAD 1 START *****
- ************************************************/
+/**************************
+ ******THREAD 1 START *****
+ *************************/
 void put_buff1(char * userInput) {
    strcpy(buff1[line1], userInput);
    line1++;
@@ -177,9 +180,9 @@ void* getUserInput() {
       }
    }
 }
-/***********************************************
- ******* ACTIONS PERFORMED BY THREAD 1 END *****
-***********************************************/
+/****************************
+ ******* THREAD 1 END *******
+****************************/
 
 
 
@@ -193,14 +196,16 @@ void print_buff2() {
    for(int i = 0; i < line2; i++) {
       printf("Buffer2[%i]: %s", i, buff2[i]);
    }
-   printf("Line 3: %i\n", line3);
+   //printf("Line 3: %i\n", line3);
 }
 
 void print_buff3() {
    for(int i = 0; i < line3; i++) {
-      printf("Buffer3[%i]: %s", i, buff3[i]);
+      printf("Buffer3[%i]: %s", i+1, buff3[i]);
+      fflush(stdout);
    }
    printf("\n");
+   fflush(stdout);
 }
 
 int main() {
@@ -209,17 +214,18 @@ int main() {
    pthread_create(&input_t, NULL, getUserInput, NULL);
    pthread_create(&line_t, NULL, separateLine, NULL);
    pthread_create(&plus_t, NULL, changePlus, NULL);
-   //pthread_create(&output_t, NULL, printBuff, NULL);
+   pthread_create(&output_t, NULL, printBuff, NULL);
    
    
    pthread_join(input_t, NULL);
    pthread_join(line_t, NULL);
    pthread_join(plus_t, NULL);
-   //pthread_join(output_t, NULL);
-   
-   //changePlus();
+   pthread_join(output_t, NULL);
 
-   print_buff2();
+   //print_buff1();
+   //print_buff2();
+   printf("*** BUFFER 3 ***\n");
+   fflush(stdout);
    print_buff3();
    return 0;
 }
